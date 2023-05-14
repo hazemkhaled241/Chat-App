d93c08bb75285bcaf63723abb38386a160c7f2bf
hazem khaled <hazemk778@gmail.com>
2023-05-14 23:48:04 +0300

    mapper created and make some changes at build gradle

---
+++ app/src/main/java/com/hazem/chat/data/mapper/Mapper.kt
@@ -0,0 +1,46 @@
+package com.hazem.chat.data.mapper
+
+import android.net.Uri
+import com.hazem.chat.data.remote.dto.MessageDto
+import com.hazem.chat.data.remote.dto.UserDto
+import com.hazem.chat.domain.model.Message
+import com.hazem.chat.domain.model.User
+
+fun User.toUserDto(): UserDto {
+    return UserDto(
+        userId = this.userId,
+        phoneNumber = this.phoneNumber,
+        name = this.name
+    )
+}
+
+fun UserDto.toUser(): User {
+    return User(
+        userId = this.userId,
+        phoneNumber = this.phoneNumber,
+        name = this.name
+    )
+}
+fun MessageDto.toMessage(): Message {
+    val image = if (imageUrl == null) imageUrl else Uri.parse(imageUrl)
+    return Message(
+        messageId = messageId,
+        senderId = senderId,
+        receiverId = receiverId,
+        messageText = messageText,
+        imageUrl = image,
+        date = date!!
+    )
+}
+
+fun Message.toMessageDto(): MessageDto {
+    val image = if (imageUrl == null) null else imageUrl.toString()
+    return MessageDto(
+        messageId = messageId,
+        senderId = senderId,
+        receiverId = receiverId,
+        messageText = messageText,
+        imageUrl = image,
+        date = date,
+    )
+}
