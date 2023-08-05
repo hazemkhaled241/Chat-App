72e8c986ea709a7d14e239bfb52cdfe5ce7a471d
hazem khaled <hazemk778@gmail.com>
2023-08-05 21:48:35 +0300

    add uri to User class and url to UserDto class

--- app/src/main/java/com/hazem/chat/data/remote/dto/UserDto.kt
+++ app/src/main/java/com/hazem/chat/data/remote/dto/UserDto.kt
@@ -3,5 +3,6 @@ package com.hazem.chat.data.remote.dto
 data class UserDto (
     val userId:String="",
     val phoneNumber:String="",
-    val name:String=""
+    val name:String="",
+    val url:String=""
 )
--- app/src/main/java/com/hazem/chat/domain/model/User.kt
+++ app/src/main/java/com/hazem/chat/domain/model/User.kt
@@ -1,5 +1,6 @@
 package com.hazem.chat.domain.model
 
+import android.net.Uri
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
@@ -7,5 +8,6 @@ import kotlinx.android.parcel.Parcelize
 data class User(
     var userId:String,
     val phoneNumber:String,
-    val name:String
+    val name:String,
+    val uri:Uri
 ):Parcelable
