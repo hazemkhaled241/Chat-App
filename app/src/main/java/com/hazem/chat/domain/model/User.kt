7116e792ce07eb5d8be91e989b9a70acc2f04f9f
hazem khaled <hazemk778@gmail.com>
2023-08-07 18:22:57 +0300

    edit the package name of the parcelable annotation

--- app/src/main/java/com/hazem/chat/domain/model/User.kt
+++ app/src/main/java/com/hazem/chat/domain/model/User.kt
@@ -2,7 +2,7 @@ package com.hazem.chat.domain.model
 
 import android.net.Uri
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class User(
